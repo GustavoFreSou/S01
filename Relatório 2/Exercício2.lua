2b7b5076211852293b1b102cdd9661a0d1b75d9a
Gustavo Sousa <gusta.frei.sou@gmail.com>
2026-08-26 15:29:51 -0300

    Update Exercício2.lua

--- Relatório 2/Exercício2.lua
+++ Relatório 2/Exercício2.lua
@@ -1,1 +1,26 @@
+print("Digite a quantidade de elementos (N):")
+local n = tonumber(io.read())
+local contador = 0
+local elementos = {}
 
+for i = 1, n do
+    print("Digite o " .. i .. ":")
+    elementos[i] = tonumber(io.read())
+
+end
+
+print("Digite o número X a ser buscado:")
+local x = tonumber(io.read())
+
+for i = 1, n do
+    if elementos[i] == x then
+        contador = contador + 1
+    end
+    
+end
+
+if contador > 0 then
+    print("O número " .. x .. " aparece " .. contador .. " vez(es) na tabela.")
+else
+    print("Numero não encontrado")
+end
