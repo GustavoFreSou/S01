54f0f526e49d2cc7556102f34715ca166bdc1fad
Gustavo Sousa <gusta.frei.sou@gmail.com>
2026-09-10 21:41:10 -0300

    Update Exercício4.go

--- Relatório 4/Exercício4.go
+++ Relatório 4/Exercício4.go
@@ -1,1 +1,29 @@
+package main
+import "fmt"
 
+func validarIngresso(setor string, codigo int) bool {
+	if setor == "VIP" && codigo == 2026 {
+		return true
+	}
+	return false
+}
+func main() {
+	var setor string
+	var codigo int
+
+	for {
+		fmt.Print("Digite o setor do ingresso: \n")
+		fmt.Scanln(&setor)
+
+		fmt.Print("Digite o codigo do ingresso: \n")
+		fmt.Scanln(&codigo)
+
+		if validarIngresso(setor, codigo) {
+			fmt.Println("Acesso liberado a area VIP!")
+			break
+		} else {
+			fmt.Println("Ingresso ou setor invalido. Tente novamente.")
+		}
+		fmt.Println()
+	}
+}
